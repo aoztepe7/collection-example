4045835a1d507b06d72518356d3274d95ed1c364
Ali Oztepe <aliztp7@gmail.com>
2020-07-18 19:10:36 +0300

    initial commit

---
+++ src/main/java/com/ztp/collectionexample/CollectionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ztp.collectionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionExampleApplication.class, args);
+    }
+
+}
